2b0dd27492594ee525eab38f6f4428d596d31207
datchung <innochi.contact@gmail.com>
2015-06-18 23:59:57 -0400

    Test that getOptions returns default options when none are provided

--- tests/tests.js
+++ tests/tests.js
@@ -1,3 +1,12 @@
-QUnit.test( "hello test", function( assert ) {
-  assert.ok( 1 == "1", "Passed!" );
+QUnit.test( "getOptions default", function( assert ) {
+
+    var options = $.fn.inputFileText.getOptions();
+
+    assert.equal(options.text, 
+        'Choose File', 
+        'Should return default text option when no text option is provided.');
+
+    assert.equal(options.remove, 
+        false, 
+        'Should return default remove option when no text option is provided.');
 });
