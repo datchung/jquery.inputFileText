38b2bca66eec3a88a7e0015f1ef9e849f389b51d
datchung <innochi.contact@gmail.com>
2015-06-18 22:57:30 -0400

    Don't allow plugin to be applied to same input file element more than once

--- src/jquery-input-file-text.js
+++ src/jquery-input-file-text.js
@@ -4,6 +4,13 @@
 (function($) {
  
     $.fn.inputFileText = function(userOptions) {
+        var MARKER_ATTRIBUTE = 'data-inputFileText';
+
+        if(this.attr(MARKER_ATTRIBUTE) === 'true') {
+            // Plugin has already been applied to input file element
+            return this;
+        }
+
         var options = $.extend({
             // Defaults
             text: 'Choose File',
@@ -23,7 +30,7 @@
 
         // Insert text after button element
         var text = $(
-            '<input type="text" style="readonly:true; border:none;" />'
+            '<input type="text" style="readonly:true; border:none; margin-left: 5px" />'
             ).insertAfter(button);
 
         // Open input file dialog when button clicked
@@ -37,7 +44,8 @@
             text.val(self.val());
         });
  
-        return this;
+        // Mark that this plugin has been applied to the input file element
+        return this.attr(MARKER_ATTRIBUTE, 'true');
     };
  
 }(jQuery));
