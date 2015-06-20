512add7bd9a592d25fcf0fee5c2a37acf540ea74
datchung <innochi.contact@gmail.com>
2015-06-19 23:01:03 -0400

    Test that getOptions returns user options when they are provided

--- tests/tests.js
+++ tests/tests.js
@@ -1,4 +1,6 @@
-QUnit.test( "getOptions default", function( assert ) {
+QUnit.module('getOptions');
+
+QUnit.test("Default", function(assert) {
 
     var options = $.fn.inputFileText.getOptions();
 
@@ -8,5 +10,23 @@ QUnit.test( "getOptions default", function( assert ) {
 
     assert.equal(options.remove, 
         false, 
-        'Should return default remove option when no text option is provided.');
+        'Should return default remove option when no remove option is provided.');
+});
+
+QUnit.test("User provided", function(assert) {
+
+    var text = 'Select File';
+    var remove = true;
+    var options = $.fn.inputFileText.getOptions({
+        text: text,
+        remove: remove
+    });
+
+    assert.equal(options.text, 
+        text, 
+        'Should return user\'s text option when it is provided.');
+
+    assert.equal(options.remove, 
+        remove, 
+        'Should return user\'s remove option when it is provided.');
 });
